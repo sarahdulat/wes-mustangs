af2a05ad76b130829d3ed0b1c92b539e96e04f42
Sarah Dulat <sarah.dulat@gmail.com>
2016-03-24 16:15:58 -0400

    add styles and contact page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-$(document).ready {
+$('document').ready(function() {
+
+		//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.backToTop').fadeIn();
+		} else {
+			$('.backToTop').fadeOut();
+		}
+	});
 	
-}
+	//Click event to scroll to top
+	$('.backToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+	$('#submit').click(function(){
+		alert("Thank you for getting in touch!");
+	});
+
+});
